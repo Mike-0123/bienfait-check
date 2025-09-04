3aa9ae92d2a850694905847c94d34fcc5bd3d6a0
Mike-0123 <k1miketechkaka@gmail.com>
2025-09-04 14:12:23 +0200

    verfied

--- src/components/user/common/nav/OwnerInfo.tsx
+++ src/components/user/common/nav/OwnerInfo.tsx
@@ -1,5 +1,6 @@
 import { FaEnvelope, FaPhoneAlt } from 'react-icons/fa'
 import { FaLocationPin } from 'react-icons/fa6'
+import { BsPatchCheckFill } from 'react-icons/bs' // better verified badge
 import constants from '../../../../constants'
 import { Link } from 'react-router-dom'
 
@@ -9,6 +10,15 @@ const OwnerInfo = () => {
                 <Link className="flex gap-2 items-center" target='_blank' to={`tel:+250${constants.webInfo.phone}`} ><FaPhoneAlt /> <span>+(250){constants.webInfo.phone}</span></Link>
                 <div className="flex gap-2 items-center"><FaLocationPin /> <span>{constants.webInfo.location}</span></div>
                 <Link className="hidden md:flex gap-2 items-center" target='_blank' to={`mailto:${constants.webInfo.email}`}><FaEnvelope /> <span>{constants.webInfo.email}</span></Link>
+                
+                {/* Verified badge */}
+                <Link 
+                    className="hidden md:flex items-center gap-1 px-2 py-0.5 bg-white border border-red-600 text-red-600 rounded-full font-semibold text-xs"
+                    target='_blank' 
+                    to="https://www.rdb.rw/investors/verified-companies/"
+                >
+                    <BsPatchCheckFill size={16} /> <span>Verified</span>
+                </Link>
         </div>
     )
 }
--- src/components/user/common/shared/SocialMedia.tsx
+++ src/components/user/common/shared/SocialMedia.tsx
@@ -1,11 +1,13 @@
 import { Link } from 'react-router-dom'
 import { FaFacebook, FaInstagram, FaTwitter } from "react-icons/fa"
+import { BsPatchCheckFill } from "react-icons/bs" // better verified badge
 import { ISocialMedia } from '../../../types'
 
 export const socialMediaLinks = {
     twitter: 'https://x.com/SpartialEnginer',
     facebook: 'https://www.facebook.com/profile.php?id=100077757162113',
-    instagram: 'https://www.instagram.com/spartial_engineering_group'
+    instagram: 'https://www.instagram.com/spartial_engineering_group',
+    verified: 'https://www.rdb.rw/investors/verified-companies/'
 }
 
 const socialMedias: ISocialMedia[] = [
@@ -21,8 +23,11 @@ const socialMedias: ISocialMedia[] = [
         icon: <FaInstagram size={24} />,
         lnk: socialMediaLinks.instagram
     },
+    {
+        icon: <BsPatchCheckFill size={24} className="text-blue-400" />, // official verified badge look
+        lnk: socialMediaLinks.verified
+    },
 ]
-
 
 const SocialMedia = ({className}: {className?: string}) => {
   return (
